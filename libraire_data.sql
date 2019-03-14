d3e320b5aa07f50d03469d3551279c82c3756dcb
Thavethini <47026139+Thavethini@users.noreply.github.com>
2019-03-14 12:10:18 +0100

    Create libraire_data.sql

---
+++ libraire_data.sql
@@ -0,0 +1,20 @@
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/customers.csv'
+INTO TABLE customers
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(client_id, sex, birth);
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/products.csv'
+INTO TABLE products
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(id_prod, price, categ);
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/transactions.csv'
+INTO TABLE transactions
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(id_prod, date_transactions, session_id, client_id);
